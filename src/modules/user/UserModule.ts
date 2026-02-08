87c196273f753e751ee6e3bdcdca98356856548d
Guillermo Díaz <alu0101505688@ull.edu.es>
2026-02-08 20:16:00 +0000

    feat: refactor login action to use dependency injection and improve error handling

---
+++ src/modules/user/UserModule.ts
@@ -0,0 +1,16 @@
+import { Token } from "@/modules/shared/domain/Token";
+import { NextCookieService } from "@/modules/shared/infrastructure/services/NextCookieService";
+import { LoginUser } from "@/modules/user/application/use-cases/LoginUser";
+import { HttpUserRepository } from "@/modules/user/infrastructure/repositories/HttpUserRepository";
+import { Container } from "inversify";
+
+const container: Container = new Container();
+
+// Repositories
+container.bind(Token.USER_REPOSITORY).to(HttpUserRepository);
+container.bind(Token.COOKIES_SERVICE).to(NextCookieService);
+
+// Use cases
+container.bind(LoginUser).toSelf();
+
+export const loginUserUseCase = container.get(LoginUser);
--- src/modules/user/application/use-cases/LoginUser.ts
+++ src/modules/user/application/use-cases/LoginUser.ts
@@ -1,7 +1,12 @@
-import { UserRepository } from "@/modules/user/domain/repositories/UserRepository";
+import { Token } from "@/modules/shared/domain/Token";
+import type { UserRepository } from "@/modules/user/domain/repositories/UserRepository";
+import { inject } from "inversify";
 
 export class LoginUser {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    @inject(Token.USER_REPOSITORY)
+    private readonly userRepository: UserRepository,
+  ) {}
 
   async run(email: string, password: string) {
     await this.userRepository.login({ email, password });
