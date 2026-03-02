b8cac3a1e79139dc13711209da4b49e6f6794f64
Guillermo Díaz <alu0101505688@ull.edu.es>
2026-03-02 20:35:20 +0000

    feat: enhance NavBar with internationalization and update styles

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,6 +2,7 @@
 
 import { UserProfile } from "@/components/UserProfile";
 import { AppSectionsRoutes } from "@/types/AppSectionsRoutes";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
@@ -9,6 +10,8 @@ import styles from "./NavBar.module.css";
 
 export const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const t = useTranslations("navBar");
 
   const toggleMenu = () => {
     setIsMenuOpen((currentState) => !currentState);
@@ -31,11 +34,7 @@ export const NavBar = () => {
         <h2>Rondo</h2>
       </Link>
 
-      <button
-        type="button"
-        className={styles.menuButton}
-        onClick={toggleMenu}
-      >
+      <button type="button" className={styles.menuButton} onClick={toggleMenu}>
         <span className={styles.menuIcon} />
         <span className={styles.menuIcon} />
         <span className={styles.menuIcon} />
@@ -45,16 +44,16 @@ export const NavBar = () => {
         className={`${styles.navLinks} ${isMenuOpen ? styles.open : ""}`.trim()}
       >
         <Link href={AppSectionsRoutes.HOME} onClick={closeMenu}>
-          Home
+          {t("navSections.home")}
         </Link>
         <Link href={AppSectionsRoutes.CREATE} onClick={closeMenu}>
-          Create
+          {t("navSections.create")}
         </Link>
         <Link href={AppSectionsRoutes.COMMUNITY} onClick={closeMenu}>
-          Community
+          {t("navSections.community")}
         </Link>
         <Link href={AppSectionsRoutes.MY_TACTICS} onClick={closeMenu}>
-          My Tactics
+          {t("navSections.myTactics")}
         </Link>
         <div className={styles.mobileUserProfile}>
           <UserProfile />
