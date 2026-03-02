4a4cb74c5e7824e8098b2d0d09c1c336e4cdf498
Guillermo Díaz <alu0101505688@ull.edu.es>
2026-03-02 20:22:40 +0000

    fix: update password validation to remove minimum length requirement

--- src/features/public/auth/login/schemas/LoginSchema.ts
+++ src/features/public/auth/login/schemas/LoginSchema.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const LoginSchema = (t: (arg: string) => string) =>
   z.object({
     email: z.email({ error: t("email.invalid") }).nonempty(),
-    password: z.string().min(6, { error: t("password.invalid") }),
+    password: z.string({ error: t("password.invalid") }),
   });
 
 type LoginSchemaType = ReturnType<typeof LoginSchema>;
