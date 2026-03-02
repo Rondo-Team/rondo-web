6ff51bb3f4e66c972c35f239c52a61537c3e784f
Guillermo Díaz <alu0101505688@ull.edu.es>
2026-03-02 20:49:41 +0000

    feat: add TitleSubtitle and TrendingPlay components with styling for home page

---
+++ src/components/TitleSubtitle/index.ts
@@ -0,0 +1,1 @@
+export { TitleSubtitle } from "./TitleSubtitle";
---
+++ src/features/private/home/components/TrendingPlay/TrendingPlay.tsx
@@ -0,0 +1,10 @@
+import { TitleSubtitle } from "@/components/TitleSubtitle";
+
+export const TrendingPlay = () => {
+  return (
+    <TitleSubtitle
+      title="Trending this week"
+      subtitle="Discover the most popular play of the week"
+    />
+  );
+};
