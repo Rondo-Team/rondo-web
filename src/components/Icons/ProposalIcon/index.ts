26749e428aa9ff7417f70eb265b39652970c2e94
Guillermo Díaz <alu0101505688@ull.edu.es>
2026-03-07 19:51:07 +0000

    feat: create CommentIcon, HeartIcon, and ProposalIcon components with styling; implement TrendingPost component and update Home page

---
+++ src/components/Icons/ProposalIcon/index.ts
@@ -0,0 +1,1 @@
+export { ProposalIcon } from "./ProposalIcon";
---
+++ src/features/private/home/components/TrendingPost/components/TrendingPostContent/TrendingPostContent.tsx
@@ -0,0 +1,34 @@
+import { CommentIcon } from "@/components/Icons/CommentIcon";
+import { HeartIcon } from "@/components/Icons/HeartIcon/HeartIcon";
+import { ProposalIcon } from "@/components/Icons/ProposalIcon";
+import { TitleSubtitle } from "@/components/TitleSubtitle";
+import { getTrendingPost } from "@/features/private/home/components/TrendingPost/components/TrendingPostContent/queries/getTrendingPost";
+import styles from "./TrendingPostContent.module.css";
+
+export const TrendingPostContent = async () => {
+  const trendingPost = await getTrendingPost();
+  return (
+    <div>
+      <TitleSubtitle
+        title={trendingPost.title}
+        subtitle={trendingPost.description}
+        titleLineClamp
+        subtitleLineClamp
+      />
+      <div className={styles.postStats}>
+        <HeartIcon
+          text={trendingPost.favouritesCount.toString()}
+          color="rgba(255, 0, 0, 0.7)"
+        />
+        <CommentIcon
+          text={trendingPost.commentsCount.toString()}
+          color="rgba(0,0,0,0.7)"
+        />
+        <ProposalIcon
+          text={trendingPost.proposalsCount.toString()}
+          color="rgba(61, 118, 0, 0.7)"
+        />
+      </div>
+    </div>
+  );
+};
