c5ddebc3a48c2470b241fdd558112da53f9faffe
Guillermo Díaz <alu0101505688@ull.edu.es>
2026-03-26 11:57:25 +0000

    feat: add translations for CommunityHighlights and TrendingPost components

--- src/features/private/home/components/TrendingPost/TrendingPost.tsx
+++ src/features/private/home/components/TrendingPost/TrendingPost.tsx
@@ -1,16 +1,15 @@
 import { TitleSubtitle } from "@/components/TitleSubtitle";
 import { TrendingPostContent } from "@/features/private/home/components/TrendingPost/components/TrendingPostContent";
 import { TrendingPostContentSkeleton } from "@/features/private/home/components/TrendingPost/components/TrendingPostContentSkeleton";
+import { getTranslations } from "next-intl/server";
 import { Suspense } from "react";
 import styles from "./TrendingPost.module.css";
 
-export const TrendingPost = () => {
+export const TrendingPost = async () => {
+  const t = await getTranslations("homePage.trendingPost");
   return (
     <div className={styles.trendingPlayContainer}>
-      <TitleSubtitle
-        title="Trending this week"
-        subtitle="Discover the most popular play of the week"
-      />
+      <TitleSubtitle title={t("title")} subtitle={t("subtitle")} />
       <div className={styles.trendingPostDivisor}>
         <Suspense fallback={<TrendingPostContentSkeleton />}>
           <TrendingPostContent />
