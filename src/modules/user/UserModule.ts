8f91ded90eba47e81bd2ee2086eef02b2898d14d
Guillermo Díaz <alu0101505688@ull.edu.es>
2026-04-02 20:17:48 +0100

    feat: add user information retrieval for navbar and related mappers

--- src/modules/user/UserModule.ts
+++ src/modules/user/UserModule.ts
@@ -1,5 +1,6 @@
 import { Token } from "@/modules/shared/domain/Token";
 import { NextCookieService } from "@/modules/shared/infrastructure/services/NextCookieService";
+import { GetUserNavBarInfo } from "@/modules/user/application/use-cases/GetUserNavBarInfo";
 import { GetUserRecentPlays } from "@/modules/user/application/use-cases/GetUserRecentPlays";
 import { LoginUser } from "@/modules/user/application/use-cases/LoginUser";
 import { RegisterUser } from "@/modules/user/application/use-cases/RegisterUser";
@@ -16,7 +17,9 @@ container.bind(Token.COOKIES_SERVICE).to(NextCookieService);
 container.bind(LoginUser).toSelf();
 container.bind(RegisterUser).toSelf();
 container.bind(GetUserRecentPlays).toSelf();
+container.bind(GetUserNavBarInfo).toSelf();
 
 export const loginUserUseCase = container.get(LoginUser);
 export const registerUserUseCase = container.get(RegisterUser);
 export const getUserRecentPlaysUseCase = container.get(GetUserRecentPlays);
+export const getUserNavBarInfoUseCase = container.get(GetUserNavBarInfo);
