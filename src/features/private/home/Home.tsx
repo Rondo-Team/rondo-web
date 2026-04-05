c0947a725127696855d8713efbe47cdfe364efe3
Guillermo Díaz <alu0101505688@ull.edu.es>
2026-04-05 11:58:39 +0100

    feat: add TacticBoard component with styling and integrate into Home

--- src/features/private/home/Home.tsx
+++ src/features/private/home/Home.tsx
@@ -1,3 +1,4 @@
+import { TacticBoard } from "@/components/TacticBoard";
 import { CommunityHighlights } from "@/features/private/home/components/CommunityHighlights";
 import { QuickAccess } from "@/features/private/home/components/QuickAccess/QuickAccess";
 import { TrendingPost } from "@/features/private/home/components/TrendingPost";
@@ -6,6 +7,7 @@ import styles from "./Home.module.css";
 export const Home = () => {
   return (
     <div className={styles.homeContainer}>
+      <TacticBoard />
       <TrendingPost />
       <CommunityHighlights />
       <QuickAccess />
