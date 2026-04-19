e84cad9e69a358eb8dded54c129d9660aa7be6d8
Guillermo Díaz <alu0101505688@ull.edu.es>
2026-04-19 13:54:05 +0100

    feat: add character limit validation for title and description in CreateContent and EditableTextField components

--- src/features/private/home/components/TrendingPost/components/TrendingPostContent/TrendingPostContent.tsx
+++ src/features/private/home/components/TrendingPost/components/TrendingPostContent/TrendingPostContent.tsx
@@ -3,13 +3,14 @@ import { getTrendingPost } from "@/features/private/home/components/TrendingPost
 import { AppSectionsRoutes } from "@/types/AppSectionsRoutes";
 import { getTranslations } from "next-intl/server";
 import Link from "next/link";
+import styles from "./TrendingPostContent.module.css"
 
 export const TrendingPostContent = async () => {
   const t = await getTranslations("homePage.trendingPost");
   const trendingPost = await getTrendingPost();
   if (!trendingPost) return <p>{t("error")}</p>;
   return (
-    <Link href={`${AppSectionsRoutes.POST}/${trendingPost.id}`}>
+    <Link href={`${AppSectionsRoutes.POST}/${trendingPost.id}`} className={styles.postContainer}>
       <PostLargeCard
         title={trendingPost.title}
         description={trendingPost.description}
--- src/modules/shared/domain/consts.ts
+++ src/modules/shared/domain/consts.ts
@@ -1,2 +1,7 @@
 export const COMMUNITY_HIGHLIGHT_DEFAULT_PAGE = 1;
 export const COMMUNITY_HIGHLIGHT_DEFAULT_LIMIT = 3;
+
+export const CREATE_PLAY_LIMITS = {
+  title: { min: 3, max: 30 },
+  description: { min: 10, max: 500 },
+} as const;
