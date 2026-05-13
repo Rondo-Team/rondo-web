795cc740a73f5b9770226c6d34084b137b29f0be
Guillermo Díaz <alu0101505688@ull.edu.es>
2026-05-13 13:52:18 +0100

    feat: add use-debounce for optimized search handling in CommunitySearch component

--- src/features/private/community/components/CommunityContent/components/CommunitySearch/CommunitySearch.tsx
+++ src/features/private/community/components/CommunityContent/components/CommunitySearch/CommunitySearch.tsx
@@ -2,18 +2,19 @@
 import { SearchBar } from "@/components/SearchBar";
 import { SearchPostQueryParams } from "@/types/SearchPostQueryParams";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export const CommunitySearch = () => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = (term: string) => {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
     if (term) params.set(SearchPostQueryParams.QUERY, term);
     else params.delete(SearchPostQueryParams.QUERY);
     replace(`${pathname}?${params.toString()}`);
-  };
+  }, 300);
 
   // Faltaria todavia añadir aqui los filtros, y que se vean reflejados en la url.
   return (
@@ -21,7 +22,9 @@ export const CommunitySearch = () => {
       <SearchBar
         placeholder="Search for posts"
         onChange={handleSearch}
-        defaultValue={searchParams.get(SearchPostQueryParams.QUERY?.toString())}
+        defaultValue={
+          searchParams.get(SearchPostQueryParams.QUERY?.toString()) ?? ""
+        }
       />
     </div>
   );
